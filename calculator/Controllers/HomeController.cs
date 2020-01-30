eb77b6e4b65c760a5db75c5844f6ff17ac2bc95a
student <student@kr.step>
2020-01-30 19:29:47 +0200

    first commit

---
+++ calculator/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System.Web.Mvc;
+namespace calculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Viewcalcul()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Viewcalcul(int a, int b, string param)
+        {
+            ViewBag.A = a;
+            ViewBag.B = b;
+            ViewBag.Param = param;
+
+            if (param == "+")
+            {
+                int res = a + b;
+                ViewBag.Sum = res;
+            }
+            else if (param == "-")
+            {
+                int res = a - b;
+                ViewBag.Sum = res;
+            }
+            else if (param == "*")
+            {
+                int res = a * b;
+                ViewBag.Sum = res;
+            }
+            else if (param == "/")
+            {
+                int res = a / b;
+                ViewBag.Sum = res;
+
+            }
+            return View();
+
+
+
+
+
+        }
+
+
+    }
+}
